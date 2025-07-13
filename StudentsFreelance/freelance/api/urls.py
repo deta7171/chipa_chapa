406736af9c3255211e9faad40d8d5150a037f4dd
user <user@kali.kali>
2025-07-13 12:24:21 +0300

    Delete Trash

--- StudentsFreelance/freelance/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import AddCommentsView, AddOrderView
-
-
-urlpatterns = [
-    path('add-comment/', AddCommentsView.as_view()),
-    path('add-order/', AddOrderView.as_view())
-]
