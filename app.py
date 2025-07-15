2edd33a6ac5128882db928afc89aa3b2e624d4f8
user <user@kali.kali>
2025-07-15 12:35:29 +0300

    aa

--- app.py
+++ app.py
@@ -39,7 +39,39 @@ def home():
         item.append(el[2])
         namesForm3.append(item)
 
-    return render_template('elements.html',data=namesForm, data1=namesForm1, data2=namesForm2, data3=namesForm3)
+    eco=getTable('eco')
+    namesForm4=[]
+    for el in eco:
+        item=[]
+        item.append(eco.index(el))
+        item.append(el[2])
+        namesForm4.append(item)
+
+    chem=getTable('chem')
+    namesForm5=[]
+    for el in chem:
+        item=[]
+        item.append(chem.index(el))
+        item.append(el[2])
+        namesForm5.append(item)
+    
+    bio=getTable('bio')
+    namesForm6=[]
+    for el in bio:
+        item=[]
+        item.append(bio.index(el))
+        item.append(el[2])
+        namesForm6.append(item)
+
+    geogr=getTable('geogr')
+    namesForm7=[]
+    for el in geogr:
+        item=[]
+        item.append(geogr.index(el))
+        item.append(el[2])
+        namesForm7.append(item)
+
+    return render_template('elements.html',data=namesForm, data1=namesForm1, data2=namesForm2, data3=namesForm3, data4=namesForm4, data5=namesForm5, data6=namesForm6, data7=namesForm7)
 
 
 @app.route('/algeb<index>')
@@ -70,6 +102,35 @@ def infThema(index):
     tema=inf[int(index)]
     
     return render_template('forminf.html', tema3 = tema[2], form3 = tema[0], example3 = tema[1])
+
+
+@app.route('/eco<index>')
+def ecoThema(index):
+    eco=getTable('eco')
+    tema=eco[int(index)]
+    
+    return render_template('formeco.html', tema4 = tema[2], form4 = tema[0], example4 = tema[1])
+
+@app.route('/chem<index>')
+def chemThema(index):
+    chem=getTable('chem')
+    tema=chem[int(index)]
+    
+    return render_template('formchem.html', tema5 = tema[2], form5 = tema[0], example5 = tema[1])
+
+@app.route('/bio<index>')
+def bioThema(index):
+    bio=getTable('bio')
+    tema=bio[int(index)]
+    
+    return render_template('formbio.html', tema6 = tema[2], form6 = tema[0], example6 = tema[1])
+
+@app.route('/geogr<index>')
+def geogrThema(index):
+    geogr=getTable('geogr')
+    tema=geogr[int(index)]
+    
+    return render_template('formgeog.html', tema7 = tema[2], form7 = tema[0], example7 = tema[1])
 
 
 @app.route('/test')
